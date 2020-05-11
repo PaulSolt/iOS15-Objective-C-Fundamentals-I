fa51bf14c631fc2c07a7fe1b325b883616254738
Paul Solt <PaulSolt@gmail.com>
2020-05-11 12:16:47 -0400

    Added starter project

---
+++ iOS15-Objective-C-Fundamentals-I/iOS15-Objective-C-Fundamentals-I/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS15-Objective-C-Fundamentals-I
+//
+//  Created by Paul Solt on 5/11/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
